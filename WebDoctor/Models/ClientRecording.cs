100506fe29172443b57663d607d4aed3f2c3b496
Ivan Kor <kor_ivan@inbox.ru>
2018-06-12 17:06:10 +0600

    Add class ClientRecording and class clientRecordingcontext

---
+++ WebDoctor/Models/ClientRecording.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebDoctor.Models
+{
+    public class ClientRecording
+    {
+        public long Id { get; set; }
+        [Required]
+        [Display(Name = "Дата приёма")]
+        public DateTime Date { get; set; }
+        [Required]
+        [Display(Name = "Время приёма")]
+        public TimeSpan Time { get; set; }
+        [Required]
+        [Display(Name = "Категория врача")]
+        public string Specialisation { get; set; }
+        [Required]
+        [Display(Name = "Имя врача")]
+        public string DoctorName { get; set; }
+        [Display(Name = "Дополнительная информация")]
+        public string AdditionalInfo { get; set; }
+        [Display(Name = "Статус приёма")]
+        public bool Status { get; set; }
+        [Display(Name = "Имя клиента")]
+        public string ClientName { get; set; }
+        [Display(Name = "Диагноз")]
+        public string Diagnos { get; set; }
+    }
+}
